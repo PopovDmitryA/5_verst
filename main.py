d025ba3a0be2745d0bdcf64b4cd3303fffe1cc91
PopovDmitryA <popov.dmitry.a@mail.ru>
2025-03-01 23:27:17 +0500

    add parse all protocol

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import parse_protocol
+import parse_table_protocols_in_park as ptpp
 
 def main():
     link = 'https://5verst.ru/aleksandrino/results/22.04.2023/'
     final_df_run, final_df_vol = parse_protocol.main_parse(link)
     print(final_df_run, final_df_vol)
+    link = 'https://5verst.ru/purnavolok/results/all/'
+    all_protocol = ptpp.transform_df_list_protocol(ptpp.list_protocols_in_park(link))
+    print(all_protocol)
 
 if __name__ == '__main__':
     main()
---
+++ parse_table_protocols_in_park.py
@@ -0,0 +1,55 @@
+import requests, re
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def list_protocols_in_park(link):
+    '''Парсим страницу с последними пробежками по парку'''
+    t = requests.get(link)
+    soup = BeautifulSoup(t.text, 'html.parser')
+
+    name_point_parse = soup.find_all('li', class_='menu-item menu-item-type-custom menu-item-object-custom')
+    name_point = name_point_parse[2].get_text()
+
+    table_protocols = soup.find('table', {'class': 'sortable results-table min-w-full leading-normal'})
+    rows = table_protocols.find_all('tr')
+    data = []
+    for index, row in enumerate(rows):
+        cols = [col.text.strip() for col in row.find_all(['td', 'th'])]
+        find_link = row.find('a')
+        if find_link is not None:
+            name_runner, link_runner = find_link.text, find_link.get('href')
+            cols += [name_runner, link_runner]
+        data.append(cols)
+    data[0] += ['date_event', 'link_event']
+    df = pd.DataFrame(data[1:], columns=data[0])
+    df['name_point'] = name_point
+
+    return df
+
+def transform_df_list_protocol(list_protocol):
+    '''Преобразуем таблицу в формат для БД'''
+    df_copy = list_protocol.copy()
+    df_copy = df_copy.drop(columns=['Дата'])
+
+    # Переименовываем столбцы
+    df_copy = df_copy.rename(columns={"##": "index_event",
+                                      "Финишёров": "count_runners",
+                                      "Волонтёров": "count_vol",
+                                      "Среднее время": "mean_time",
+                                      'Лучшее "Ж"': "best_time_woman",
+                                      'Лучшее "М"': "best_time_man"})
+
+    df_copy['is_test'] = df_copy['index_event'].apply(lambda x: pd.isnull(x) or x == '')
+    # Меняем порядок столбцов
+    df_copy = df_copy[["index_event",
+                       "name_point",
+                       "date_event",
+                       "link_event",
+                       "is_test",
+                       "count_runners",
+                       'count_vol',
+                       'mean_time',
+                       'best_time_woman',
+                       'best_time_man']]  # Новый порядок
+
+    return df_copy
