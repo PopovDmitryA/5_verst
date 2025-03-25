4536ed133287ae43154f25d8b54254b608dd8829
PopovDmitryA <popov.dmitry.a@mail.ru>
2025-03-25 23:40:19 +0300

    add function for record to db new data

--- update_data_functions.py
+++ update_data_functions.py
@@ -2,12 +2,14 @@ import DB_handler as db
 import pandas as pd
 import link_handler
 import parse_last_running as plr
+from tqdm import tqdm
+import parse_protocol as pp
 
-def check_new_protocols(credential):
+def check_new_protocols(engine):
     '''Получаем данные протоколов, которые можно внести в БД'''
     #Получаем данные со списком протоколов
-    engine = db.db_connect(credential)
-    df = db.get_table(engine, 'list_all_events', 'index_event, name_point, date_event, link_event, is_test')
+    #engine = db.db_connect(credential)
+    df = db.get_table(engine, 'list_all_events_copy', 'index_event, name_point, date_event, link_event, is_test')
     df['link_point'] = df['link_event'].apply(link_handler.main_link_event)
     df = df.drop(columns=['link_event'])
     DB_data = df[["index_event", "name_point", "date_event", "link_point", "is_test"]]
@@ -41,3 +43,24 @@ def check_new_protocols(credential):
                           'best_time_man']]
 
     return finish_df
+
+def add_new_protocols(credential):
+    engine = db.db_connect(credential)
+    new_data = check_new_protocols(engine)
+    if len(new_data) > 0:
+        engine = db.db_connect(credential)
+        db.append_df(engine, 'list_all_events_copy', new_data)
+        #db.info_table_update(engine, table_name, upd_time)
+        data_protocols, data_protocol_vol = pd.DataFrame(), pd.DataFrame()
+
+        count = len(new_data)
+        for index, row in tqdm(new_data.iterrows(), total=count):
+            link = row['link_event']
+            final_df_run, final_df_vol = pp.main_parse(link)
+            data_protocols = pd.concat([data_protocols, final_df_run], ignore_index=True)
+            data_protocol_vol = pd.concat([data_protocol_vol, final_df_vol], ignore_index=True)
+
+        engine = db.db_connect(credential)
+        db.append_df(engine, 'details_protocol_copy', data_protocols)
+        engine = db.db_connect(credential)
+        db.append_df(engine, 'details_vol_copy', data_protocol_vol)
