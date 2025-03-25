5d819dfd66b616cda1023b1e2a16df687caa0b87
PopovDmitryA <popov.dmitry.a@mail.ru>
2025-03-25 23:38:36 +0300

    add function record to db dataframe

--- DB_handler.py
+++ DB_handler.py
@@ -1,4 +1,6 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 import pandas as pd
 
 def db_connect(credential):
@@ -10,3 +12,19 @@ def get_table(engine, name_table, columns='*'):
     '''Считать таблицу из БД'''
     table = pd.read_sql(f"SELECT {columns} FROM {name_table}", con=engine)
     return table
+
+def append_df(engine, table, df):
+    df.to_sql(table, engine, if_exists="append", index=False)
+    info_table_update(engine, table, datetime.now())
+
+def info_table_update(engine, table_name, upd_time):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    insert_query = sa.text(f"""
+        INSERT INTO update_table (table_name, update_date)
+        VALUES ('{table_name}', '{upd_time}');
+        """)
+    session.execute(insert_query)
+    session.commit()
+    session.close()
