d1d3f8cea94ad70a9fe4631ba0b0b59c1d77ab0f
PopovDmitryA <popov.dmitry.a@mail.ru>
2025-03-26 22:37:13 +0300

    minor fix + add more print infromation about progress running script

--- DB_handler.py
+++ DB_handler.py
@@ -3,19 +3,23 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 import pandas as pd
 
+
 def db_connect(credential):
     '''Подключение в базе данных'''
     engine = sa.create_engine(credential)
     return engine
+
 
 def get_table(engine, name_table, columns='*'):
     '''Считать таблицу из БД'''
     table = pd.read_sql(f"SELECT {columns} FROM {name_table}", con=engine)
     return table
 
+
 def append_df(engine, table, df):
     df.to_sql(table, engine, if_exists="append", index=False)
     info_table_update(engine, table, datetime.now())
+
 
 def info_table_update(engine, table_name, upd_time):
     Session = sessionmaker(bind=engine)
